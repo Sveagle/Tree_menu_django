7d15a99555c732dcf8aadc0a575507814b2fd681
Sviatoslav Orel <mrsveagle@gmail.com>
2025-07-26 14:55:21 +0300

    added verbose_name

--- tree_menu_test/tree_menu/apps.py
+++ tree_menu_test/tree_menu/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TreeMenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tree_menu'
+    verbose_name = '🌳 Древовидные меню'
--- tree_menu_test/tree_menu_test/settings.py
+++ tree_menu_test/tree_menu_test/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tree_menu'
+    'tree_menu.apps.TreeMenuConfig',
 ]
 
 MIDDLEWARE = [
